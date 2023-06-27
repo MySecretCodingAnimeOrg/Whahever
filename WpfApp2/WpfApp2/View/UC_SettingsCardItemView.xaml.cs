1e7622086d8643881007a7571ca2d7a5fc734033
Pahomix <112718432+Pahomix@users.noreply.github.com>
2023-06-27 23:39:49 +0300

    WHAH-20

--- WpfApp2/WpfApp2/View/UC_SettingsCardItemView.xaml.cs
+++ WpfApp2/WpfApp2/View/UC_SettingsCardItemView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp2.ViewModel;
 
 namespace WpfApp2.View
 {
@@ -23,6 +24,7 @@ namespace WpfApp2.View
         public UC_SettingsCardItemView()
         {
             InitializeComponent();
+            DataContext = new UC_SettingsCardItemViewModel();
         }
     }
 }
